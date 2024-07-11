960c5aea3eeae457db5fa67b6aa6e3ab941017ae
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-07-11 16:34:48 +1000

    database init

--- src/main/java/net/worldmc/merovingian/Merovingian.java
+++ src/main/java/net/worldmc/merovingian/Merovingian.java
@@ -3,26 +3,40 @@ package net.worldmc.merovingian;
 import net.worldmc.merovingian.listeners.PlayerChatListener;
 import net.worldmc.merovingian.listeners.PlayerDeathListener;
 import net.worldmc.merovingian.listeners.PlayerJoinListener;
-import net.worldmc.merovingian.managers.DeathInventoryManager;
-import net.worldmc.merovingian.managers.WelcomeRewardManager;
+import net.worldmc.merovingian.listeners.VoteListener;
+import net.worldmc.merovingian.managers.DatabaseManager;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Merovingian extends JavaPlugin {
+    private static Merovingian instance;
+    private DatabaseManager databaseManager;
 
     @Override
     public void onEnable() {
+        instance = this;
         saveDefaultConfig();
 
-        WelcomeRewardManager welcomeRewardManager = new WelcomeRewardManager(this);
-        DeathInventoryManager deathInventoryManager = new DeathInventoryManager();
+        databaseManager = new DatabaseManager(this);
+        databaseManager.connect();
 
-        getServer().getPluginManager().registerEvents(new PlayerDeathListener(deathInventoryManager), this);
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(welcomeRewardManager, this), this);
-        getServer().getPluginManager().registerEvents(new PlayerChatListener(welcomeRewardManager), this);
+        PluginManager pluginManager = getServer().getPluginManager();
+        pluginManager.registerEvents(new PlayerDeathListener(this), this);
+        pluginManager.registerEvents(new PlayerJoinListener(this), this);
+        pluginManager.registerEvents(new PlayerChatListener(this), this);
+        pluginManager.registerEvents(new VoteListener(this), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Merovingian getInstance() {
+        return instance;
+    }
+
+    public DatabaseManager getDatabaseManager() {
+        return databaseManager;
     }
 }
